a06f0d89967aba13a1d5b3ee9cca6dd0e0e8aaae
Geoffrey Emerson <geoffrey.emerson.git@gmail.com>
2018-08-02 17:39:04 -0700

    demo code

---
+++ main.js
@@ -0,0 +1,169 @@
+process.env.NODE_ENV = 'production';
+
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+let mainWindow;
+let addWindow;
+
+// Listen for app to be ready
+app.on('ready', function() {
+    // Create new window
+    mainWindow = new BrowserWindow({});
+    // Load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // Quit app when closed
+    mainWindow.on('closed', function() {
+        app.quit();
+    })
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
+});
+
+// Handle create add window
+function createAddWindow() {
+    // Create new window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Shopping List Item'
+    });
+    // Load html into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    // A little garbage collection
+    addWindow.on('close', function () {
+        addWindow = null;
+    })
+}
+
+// Catch item:add
+ipcMain.on('item:add', function(event, item){
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
+})
+
+// Create menu template
+const mainMenuTemplate = [
+    {
+      label: 'File',
+      submenu: [
+        {
+            label: 'Add Item',
+            click() {
+                createAddWindow();
+            }
+        },
+        {
+            label: 'Clear Items',
+            click() {
+                mainWindow.webContents.send('item:clear');
+            }
+        }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {role: 'undo'},
+        {role: 'redo'},
+        {type: 'separator'},
+        {role: 'cut'},
+        {role: 'copy'},
+        {role: 'paste'},
+        {role: 'pasteandmatchstyle'},
+        {role: 'delete'},
+        {role: 'selectall'}
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {role: 'reload'},
+        {type: 'separator'},
+        {role: 'resetzoom'},
+        {role: 'zoomin'},
+        {role: 'zoomout'},
+        {type: 'separator'},
+        {role: 'togglefullscreen'}
+      ]
+    },
+    {
+      role: 'window',
+      submenu: [
+        {role: 'minimize'},
+        {role: 'close'}
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click () { require('electron').shell.openExternal('https://electron.atom.io') }
+        }
+      ]
+    }
+];
+
+if (process.platform === 'darwin') {
+    mainMenuTemplate.unshift({
+        label: app.getName(),
+        submenu: [
+            {role: 'about'},
+            {type: 'separator'},
+            {role: 'services', submenu: []},
+            {type: 'separator'},
+            {role: 'hide'},
+            {role: 'hideothers'},
+            {role: 'unhide'},
+            {type: 'separator'},
+            {role: 'quit'}
+        ]
+    });
+
+    // Edit menu
+    mainMenuTemplate[2].submenu.push(
+        {type: 'separator'},
+        {
+            label: 'Speech',
+            submenu: [
+                {role: 'startspeaking'},
+                {role: 'stopspeaking'}
+            ]
+        }
+    )
+
+    // Window menu
+    mainMenuTemplate[4].submenu = [
+        {role: 'close'},
+        {role: 'minimize'},
+        {role: 'zoom'},
+        {type: 'separator'},
+        {role: 'front'}
+    ]
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {role: 'toggledevtools'},
+            {role: 'forcereload'},
+        ]
+    })
+}
